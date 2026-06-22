c38a4c1ab0c5753f7b7118f23c881a7387d4a891
anupsr <sranup007@gmail.com>
2026-06-22 13:10:17 +0530

    new chnages

--- src/App.jsx
+++ src/App.jsx
@@ -67,42 +67,42 @@
 
 // export default App;
 
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home/Home';
-import AboutUs from './pages/Company/AboutUs';
-import BlogDetail from './pages/Blog/BlogDetail';
-import BlogGird from './pages/Blog/BlogGird';
-import BlogListing from './pages/Blog/BlogListing';
-import Contact from './pages/Company/Contact';
-import Faqs from './pages/Company/Faqs';
-import HomeBrandingAgency from './pages/Home/HomeBrandingAgency';
-import HomeSeo from './pages/Home/HomeSeo';
-import HomeSeoMarketing from './pages/Home/HomeSeoMarketing';
-import Index from './pages/Home/Index';
-import OurTeam from './pages/Company/OurTeam';
-import Page404 from './pages/Misc/Page404';
-import Portfolio1 from './pages/Portfolio/Portfolio1';
-import Portfolio2 from './pages/Portfolio/Portfolio2';
-import PortfolioDetails from './pages/Portfolio/PortfolioDetails';
-import PricingPlan from './pages/Company/PricingPlan';
-import ServiceDetails from './pages/Services/ServiceDetails';
-import Services1 from './pages/Services/Services1';
-import Services2 from './pages/Services/Services2';
-import TeamDetails from './pages/Company/TeamDetails';
-import SEO from './pages/Services/SEO';
-import SocialMediaMarketing from './pages/Services/SocialMediaMarketing';
-import WebDevelopment from './pages/Services/WebDevelopment';
-import GraphicDesign from './pages/Services/GraphicDesign';
-import LeadGeneration from './pages/Services/LeadGeneration';
-import WhatsappAutomation from './pages/Services/WhatsappAutomation';
-import GraphicDesignServiceDetails from './pages/Services/ServiceDetails/GraphicDesignServiceDetails'
-import DigitalMarketingServiceDetails from './pages/Services/ServiceDetails/DigitalMarketingServiceDetails';
-import SEOServiceDetails from './pages/Services/ServiceDetails/SEOServiceDetails';
-import SocialMediaMarketingServiceDetails from './pages/Services/ServiceDetails/SocialMediaMarketingServiceDetails';
-import WebDevelopmentServiceDetails from './pages/Services/ServiceDetails/WebDevelopmentServiceDetails';
-import LeadGenerationServiceDetails from './pages/Services/ServiceDetails/LeadGenerationServiceDetails';
-import WhatsappAutomationServiceDetails from './pages/Services/ServiceDetails/WhatsappAutomationServiceDetails';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import AboutUs from "./pages/Company/AboutUs";
+import BlogDetail from "./pages/Blog/BlogDetail";
+import BlogGird from "./pages/Blog/BlogGird";
+import BlogListing from "./pages/Blog/BlogListing";
+import Contact from "./pages/Company/Contact";
+import Faqs from "./pages/Company/Faqs";
+import HomeBrandingAgency from "./pages/Home/HomeBrandingAgency";
+import HomeSeo from "./pages/Home/HomeSeo";
+import HomeSeoMarketing from "./pages/Home/HomeSeoMarketing";
+import Index from "./pages/Home/Index";
+import OurTeam from "./pages/Company/OurTeam";
+import Page404 from "./pages/Misc/Page404";
+import Portfolio1 from "./pages/Portfolio/Portfolio1";
+import Portfolio2 from "./pages/Portfolio/Portfolio2";
+import PortfolioDetails from "./pages/Portfolio/PortfolioDetails";
+import PricingPlan from "./pages/Company/PricingPlan";
+import ServiceDetails from "./pages/Services/ServiceDetails";
+import Services1 from "./pages/Services/Services1";
+import Services2 from "./pages/Services/Services2";
+import TeamDetails from "./pages/Company/TeamDetails";
+import SEO from "./pages/Services/SEO";
+import SocialMediaMarketing from "./pages/Services/SocialMediaMarketing";
+import WebDevelopment from "./pages/Services/WebDevelopment";
+import GraphicDesign from "./pages/Services/GraphicDesign";
+import LeadGeneration from "./pages/Services/LeadGeneration";
+import WhatsappAutomation from "./pages/Services/WhatsappAutomation";
+import GraphicDesignServiceDetails from "./pages/Services/ServiceDetails/GraphicDesignServiceDetails";
+import DigitalMarketingServiceDetails from "./pages/Services/ServiceDetails/DigitalMarketingServiceDetails";
+import SEOServiceDetails from "./pages/Services/ServiceDetails/SEOServiceDetails";
+import SocialMediaMarketingServiceDetails from "./pages/Services/ServiceDetails/SocialMediaMarketingServiceDetails";
+import WebDevelopmentServiceDetails from "./pages/Services/ServiceDetails/WebDevelopmentServiceDetails";
+import LeadGenerationServiceDetails from "./pages/Services/ServiceDetails/LeadGenerationServiceDetails";
+import WhatsappAutomationServiceDetails from "./pages/Services/ServiceDetails/WhatsappAutomationServiceDetails";
 
 function App() {
   return (
@@ -126,27 +126,60 @@ function App() {
         <Route path="/pricing-plan" element={<PricingPlan />} />
         <Route path="/service-details" element={<ServiceDetails />} />
 
-        <Route path="/services/Digital-marketing-agency-in-bangalore" element={<Services1 />} />
+        <Route
+          path="/services/Digital-marketing-agency-in-bangalore"
+          element={<Services1 />}
+        />
         <Route path="/services-2" element={<Services2 />} />
         <Route path="/team-details" element={<TeamDetails />} />
-        
+
         <Route path="/services/seo-services-in-bangalore" element={<SEO />} />
-        <Route path="/services/social-media-marketing-company-bangalore" element={<SocialMediaMarketing />} />
-        <Route path="/services/web-development-company-bangalore" element={<WebDevelopment />} />
-        <Route path="/services/graphic-design-company-bangalore" element={<GraphicDesign />} />
-        <Route path="/services/lead-generation-companies-in-bangalore" element={<LeadGeneration />} />
-        <Route path="/services/whatsapp-automation-AI-providers-india" element={<WhatsappAutomation />} />
+        <Route
+          path="/services/social-media-marketing-company-bangalore"
+          element={<SocialMediaMarketing />}
+        />
+        <Route
+          path="/services/web-development-company-bangalore"
+          element={<WebDevelopment />}
+        />
+        <Route
+          path="/services/graphic-design-company-bangalore"
+          element={<GraphicDesign />}
+        />
+        <Route
+          path="/services/lead-generation-companies-in-bangalore"
+          element={<LeadGeneration />}
+        />
+        <Route
+          path="/services/whatsapp-automation-AI-providers-india"
+          element={<WhatsappAutomation />}
+        />
 
-
-        <Route path="/graphic-design-service-details" element={<GraphicDesignServiceDetails />} />
-        <Route path="/digital-marketing-service-details" element={<DigitalMarketingServiceDetails />} />
+        <Route
+          path="/graphic-design-service-details"
+          element={<GraphicDesignServiceDetails />}
+        />
+        <Route
+          path="/digital-marketing-service-details"
+          element={<DigitalMarketingServiceDetails />}
+        />
         <Route path="/seo-service-details" element={<SEOServiceDetails />} />
-        <Route path="/social-media-marketing-service-details" element={<SocialMediaMarketingServiceDetails />} />
-        <Route path="/web-development-service-details" element={<WebDevelopmentServiceDetails />} />
-        <Route path="/lead-generation-service-details" element={<LeadGenerationServiceDetails />} />
-        <Route path="/whatsapp-automation-service-details" element={<WhatsappAutomationServiceDetails />} />
-        
-
+        <Route
+          path="/social-media-marketing-service-details"
+          element={<SocialMediaMarketingServiceDetails />}
+        />
+        <Route
+          path="/web-development-service-details"
+          element={<WebDevelopmentServiceDetails />}
+        />
+        <Route
+          path="/lead-generation-service-details"
+          element={<LeadGenerationServiceDetails />}
+        />
+        <Route
+          path="/whatsapp-automation-service-details"
+          element={<WhatsappAutomationServiceDetails />}
+        />
         <Route path="*" element={<Page404 />} />
       </Routes>
     </BrowserRouter>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -234,7 +234,10 @@ export default function Footer() {
                 </a>
               </li>
               <li>
-                <a href="/services/Digital-marketing-agency-in-bangalore" className="color-white ff-2">
+                <a
+                  href="/services/Digital-marketing-agency-in-bangalore"
+                  className="color-white ff-2"
+                >
                   Service
                 </a>
               </li>
